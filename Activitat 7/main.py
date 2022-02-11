9a0f5324900dc3debdff004534f46e3c3ffc2e71
DavidDev2002 <95426102+DavidDev2002@users.noreply.github.com>
2022-02-11 15:27:02 +0100

    Add files via upload

--- Activitat 7/main.py
+++ Activitat 7/main.py
@@ -1,11 +1,23 @@
 import functions as f
-#Introduce un numero natural por teclado y recibiras su numero en binario.
+#
 def main():
-    max, min = 0, 10
-    total = 0
-    size = f.tamaño()
-    for x in size
 
+    total = 0
+    notes = []
+    size = f.tamaño()
+    for i in range(size):
+        num1 = f.valor10()
+        total += num1
+        notes.append(num1)
+
+    max_value = max(notes)
+    min_value = min(notes)
+    notes.sort()
+    mitjana = f.mitjana(total, size)
+    print("\nLista ordenada: ", notes)
+    print("Mitjana dels valors introduits: ", mitjana)
+    print("Valor més gran: ", max_value)
+    print("Valor més petit: ", min_value)
 
 if __name__ == '__main__':
     main()
