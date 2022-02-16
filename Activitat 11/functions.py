376fe5ddb91b596f860384fe1cea430f0812090c
DavidDev2002 <95426102+DavidDev2002@users.noreply.github.com>
2022-02-16 15:28:18 +0100

    Add files via upload

---
+++ Activitat 11/functions.py
@@ -0,0 +1,5 @@
+def parells():
+    lista = []
+    for x in range(2, 102, 2):
+        lista.append(x)
+    return lista
---
+++ Activitat 11/main.py
@@ -0,0 +1,8 @@
+import functions as f
+#
+def main():
+
+    print(f.parells())
+
+if __name__ == '__main__':
+    main()
